0752eceb784d981ec4edcd50f40c480e822fef32
Anandhavalli G <anandhavalli004@gmail.com>
2017-06-13 11:35:46 +0530

    Initial commit

---
+++ app/src/main/java/com/anandhavalli/myhelloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package com.anandhavalli.myhelloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvHello)
+    TextView helloMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btWelcome)
+    public void welcomeAction(){
+        helloMsg.setText("HELLO WORLD");
+    }
+}
